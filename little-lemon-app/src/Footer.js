a3a5125662b8b7b1e8822c9b5a9017cd1b2a89c9
Ashraf Khan <ashrafkhan@neuracyb.com>
2024-02-23 09:10:17 +0530

    Updated links

--- little-lemon-app/src/Footer.js
+++ little-lemon-app/src/Footer.js
@@ -4,7 +4,21 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      {/* Add footer content here */}
+            Doormat Navigation
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservation">Reservation</a></li>
+            <li><a href="/order">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+
+            Contact
+        <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+        </ul>
     </footer>
   );
 };
